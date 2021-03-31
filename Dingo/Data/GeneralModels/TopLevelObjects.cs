b5b5a238d9793f2d195fabd14b3a53ba7fca2dde
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-03-31 09:28:51 -0400

    Abstracted Get Friends to TopLevel

--- Dingo/Data/GeneralModels/TopLevelObjects.cs
+++ Dingo/Data/GeneralModels/TopLevelObjects.cs
@@ -16,35 +16,89 @@ namespace Dingo
     /// </summary>
     public class TopLevelObjects : ITopLevelObjects
     {
+        /// <summary>
+        /// When added to displays and consumes the toast
+        /// </summary>
         public readonly List<ToastModel> toasts = new();
+
+        /// <summary>
+        /// When added to displays and consums the modal
+        /// </summary>
 
         public readonly List<ModalModel> modals = new();
 
-        public List<IFriendModel> Friends = new();
+        /// <summary>
+        /// The current friends of the user, is updated on log-in and checked periodically automatically
+        /// </summary>
 
-        public List<IFriendModel> FriendRequests = new();
+        public List<IFriendModel> Friends { get; set; } = new();
 
+        /// <summary>
+        /// When ran will update the the Friends list on this object
+        /// </summary>
+
+        public Func<Task> GetFriends { get; set; }
+
+        /// <summary>
+        /// List of the waiting friend requests for this user
+        /// </summary>
+
+        public List<IFriendModel> FriendRequests { get; set; } = new();
+
+        /// <summary>
+        /// Refreshes the friends requests manually
+        /// </summary>
         public Func<Task> GetFriendRequests { get; set; }
 
-        public readonly List<string> WaitingSenderIds = new();
+        /// <summary>
+        /// List of notifications that have been already sent to the user
+        /// </summary>
 
-        public readonly List<string> WaitingRequestIds = new();
+        public List<string> AlreadyPushedMessageIds { get; set; } = new();
 
+        /// <summary>
+        /// List of toasts already pushed to the user for the given ids
+        /// </summary>
+
+        public List<string> AlreadyPushedRequestIds { get; set; } = new();
+
+        /// <summary>
+        /// When invoked force updates top level state of the site
+        /// </summary>
         public Action StateHasChanged { get; set; }
 
-        public bool ShowChangeAvatar = false;
+        /// <summary>
+        /// When  true the screen will shwo the change avatar modal
+        /// </summary>
 
-        public bool ShowChangeDisplayName = false;
+        public bool ShowChangeAvatar { get; set; } = false;
 
-        public bool ShowChatInterface = false;
+        /// <summary>
+        /// When true the screen will show the change name modal
+        /// </summary>
 
+        public bool ShowChangeDisplayName { get; set; } = false;
+
+        /// <summary>
+        /// When set to true the message interface will open up for whatever the current chat id is
+        /// </summary>
+
+        public bool ShowChatInterface { get; set; } = false;
+
+        /// <summary>
+        /// The Id of the user that this user should chat with then the chat interface is shown
+        /// </summary>
         public string CurrentChatId { get; set; } = null;
 
-        private readonly ConcurrentTimerDictionary TimerDict;
+        /// <summary>
+        /// Dictionary containing all the timers that are running for this user
+        /// </summary>
+
+        private readonly ConcurrentTimerDictionary TimerDict = new();
 
         public TopLevelObjects()
         {
-            TimerDict = new();
+            // tell all timers added to the timer dict to force-update UI state when they invoke
             TimerDict.OnTimer = StateHasChanged;
         }
 
@@ -61,6 +115,7 @@ namespace Dingo
 
         public void Dispose()
         {
+            // dispose all the timers that are running when this object is disposed
             TimerDict?.Dispose();
         }
     }
