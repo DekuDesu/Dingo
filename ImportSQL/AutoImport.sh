a1a359a599ae6891869a94a32617970f19ddc535
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-04-07 11:20:02 -0400

    Fixed AUtoImport for impoting SQL automatically

--- ImportSQL/AutoImport.sh
+++ ImportSQL/AutoImport.sh
@@ -1,12 +1,12 @@
 echo "Attempting to Import Sql Scripts"
 for i in {1..50};
 do
-    
+    /opt/mssql-tools/bin/sqlcmd -S localhost,1434 -U sa -P DingoChat5% -d master -i "./CheckStatus.sql"
     if [ $? -eq 0 ]
     then
-        for entry in "SqlToImport"/*
+        for entry in "/var/opt/mssql/import/SqlToImport"/*
         do
-            /opt/mssql-tools/bin/sqlcmd -S localhost,11433 -U sa -P DingoChat5% -d master -i $entry
+            /opt/mssql-tools/bin/sqlcmd -S localhost,1434 -U sa -P DingoChat5% -d master -i $entry
             echo "Imported: "$entry
         done
         echo "Finished Importing sql scripts"
