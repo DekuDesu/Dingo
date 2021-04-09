45f9a5ff5b001c3f43e5571b823cda1aba341612
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-04-08 21:37:45 -0400

    Created Auto-Generating SQL DB

--- ImportSQL/AutoImport.sh
+++ ImportSQL/AutoImport.sh
@@ -1,18 +1,20 @@
 echo "Attempting to Import Sql Scripts"
-for i in {1..50};
+while /bin/sleep 1; 
 do
-    /opt/mssql-tools/bin/sqlcmd -S localhost,1434 -U sa -P DingoChat5% -d master -i "./CheckStatus.sql"
+    /opt/mssql-tools/bin/sqlcmd -S mssql-server,1433 -U sa -P DingoChat5% -d master -i "./CheckStatus.sql"
     if [ $? -eq 0 ]
     then
         for entry in "/var/opt/mssql/import/SqlToImport"/*
         do
-            /opt/mssql-tools/bin/sqlcmd -S localhost,1434 -U sa -P DingoChat5% -d master -i $entry
+            /opt/mssql-tools/bin/sqlcmd -S mssql-server,1433 -U sa -P DingoChat5% -d master -i $entry
             echo "Imported: "$entry
         done
         echo "Finished Importing sql scripts"
         break
     else
-        echo "Waiting on MSSQL to start before importing sqls"
-        sleep 1
+        echo " "
+        echo "Error code: 0x68 is NORMAL (failed to log in, sql not ready)"
+        echo "Waiting on MSSQL to start before importing sql"
+        echo " "
     fi
 done
